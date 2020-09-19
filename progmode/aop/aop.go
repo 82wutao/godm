cb35c6cf2161903aaff5a85b2d1dcd8f31403d9b
wutao <363697339@qq.com>
2020-09-19 19:41:59 +0800

    aop api

--- progmode/aop/aop.go
+++ progmode/aop/aop.go
@@ -5,37 +5,64 @@ import (
 	"runtime"
 )
 
-// Joinpointer 连接点
 type Joinpointer interface {
-	Name() string
-	Inputs() []reflect.Value
-	Outputs() []reflect.Value
-	Invoke() []reflect.Value
+	FuncName() string
+	FuncProtoType() reflect.Type
+
+	FuncInputs() []reflect.Value
+}
+
+// Joinpointer 连接点
+type BeforeJoinpointer interface {
+	Joinpointer
+	SetContext(ctx interface{})
+}
+type AfterJoinpointer interface {
+	Joinpointer
+	FuncOutputs() []reflect.Value
+	Context() interface{}
+}
+
+type AroundJoinpointer interface {
+	Joinpointer
+
+	SetFuncInputs([]reflect.Value)
+	InvokeFunc() []reflect.Value
+
 	Context() interface{}
 	SetContext(ctx interface{})
 }
 type joinpointerImpl struct {
-	name     string
-	inputs   []reflect.Value
-	outputs  []reflect.Value
-	function reflect.Value
-	context  interface{}
+	name      string
+	protoType reflect.Type
+	inputs    []reflect.Value
+	outputs   []reflect.Value
+	function  reflect.Value
+	context   interface{}
 }
 
-func (jpi *joinpointerImpl) Name() string {
+func (jpi *joinpointerImpl) FuncName() string {
 	return jpi.name
 }
-func (jpi *joinpointerImpl) Inputs() []reflect.Value {
+func (jpi *joinpointerImpl) FuncProtoType() reflect.Type {
+	return jpi.protoType
+}
+
+func (jpi *joinpointerImpl) FuncInputs() []reflect.Value {
 	return jpi.inputs
 }
-func (jpi *joinpointerImpl) Invoke() []reflect.Value {
-	outputs := jpi.function.Call(jpi.inputs)
-	jpi.outputs = outputs
-	return outputs
+func (jpi *joinpointerImpl) SetFuncInputs(args []reflect.Value) {
+	jpi.inputs = args
 }
-func (jpi *joinpointerImpl) Outputs() []reflect.Value {
+
+func (jpi *joinpointerImpl) InvokeFunc() []reflect.Value {
+	return jpi.function.Call(jpi.inputs)
+}
+
+func (jpi *joinpointerImpl) FuncOutputs() []reflect.Value {
 	return jpi.outputs
 }
+
 func (jpi *joinpointerImpl) Context() interface{} {
 	return jpi.context
 }
@@ -43,22 +70,24 @@ func (jpi *joinpointerImpl) SetContext(ctx interface{}) {
 	jpi.context = ctx
 }
 
-type BeforeAdvice func(jp Joinpointer)
-type AfterAdvice func(jp Joinpointer)
-type AroundAdvice func(jp Joinpointer) []reflect.Value
+type BeforeAdvice func(jp BeforeJoinpointer)
+type AfterAdvice func(jp AfterJoinpointer)
+type AroundAdvice func(jp AroundJoinpointer) []reflect.Value
 
 // CreateProxyFunc 创建一个proxy
 func CreateProxyFunc(function interface{}, before BeforeAdvice, after AfterAdvice, around AroundAdvice) interface{} {
 
+	funcPrototype := reflect.TypeOf(function)
 	proxyDef := func(inputs []reflect.Value) []reflect.Value {
 
 		rawFunc := reflect.ValueOf(function)
 		funcName := runtime.FuncForPC(rawFunc.Pointer()).Name()
 
 		jp := joinpointerImpl{
-			name:     funcName,
-			inputs:   inputs,
-			function: rawFunc,
+			name:      funcName,
+			protoType: funcPrototype,
+			inputs:    inputs,
+			function:  rawFunc,
 		}
 
 		if before != nil {
@@ -69,7 +98,7 @@ func CreateProxyFunc(function interface{}, before BeforeAdvice, after AfterAdvic
 			ret := around(&jp)
 			jp.outputs = ret
 		} else {
-			ret := jp.Invoke()
+			ret := jp.InvokeFunc()
 			jp.outputs = ret
 		}
 
@@ -80,7 +109,6 @@ func CreateProxyFunc(function interface{}, before BeforeAdvice, after AfterAdvic
 		return jp.outputs
 	}
 
-	funcPrototype := reflect.TypeOf(function)
 	proxy := reflect.MakeFunc(funcPrototype, proxyDef).Interface()
 	return proxy
 }
